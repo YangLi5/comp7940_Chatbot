eb345f43c2ca4054ab0401dd797a9aab95964c3b
zich9801 <zich5466@colorado.edu>
2022-04-21 23:53:18 +0800

    Add 24point and food
    Add 24point and food

--- chatbot.py
+++ chatbot.py
@@ -9,6 +9,15 @@ import requests
 
 from news_file import get_news_from_keyword
 
+#Chen Zixin 
+from bs4 import BeautifulSoup
+import itertools
+import random
+import time
+from firebase import firebase
+firebase = firebase.FirebaseApplication(os.environ['FireBase_url'], None)
+#Chen Zixin 
+
 global redis1
 
 def main():
@@ -35,6 +44,13 @@ def main():
     dispatcher.add_handler(CommandHandler("hello", hello))
     dispatcher.add_handler(CommandHandler("news", news))
 
+#Chen Zixin 
+    dispatcher.add_handler(CommandHandler("food", food_command))
+    dispatcher.add_handler(CommandHandler("24", twentyfour_command))
+    dispatcher.add_handler(CommandHandler("gamename", gamename_command))
+    dispatcher.add_handler(CommandHandler("yes", yes_command))
+    dispatcher.add_handler(CommandHandler("no", no_command))
+#Chen Zixin 
 
     # To start the bot:
     updater.start_polling()
@@ -84,5 +100,98 @@ def news(update: Update, context: CallbackContext) -> None:
             update.message.reply_text(news)
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /news <keyword>')
+
+
+
+
+
+
+def twentyfour_command(update:Update, context:CallbackContext)->None:
+    update.message.reply_text('Welcome to game 24 point!')
+    update.message.reply_text('Enter your game name by /gamename')
+    #update.message.reply_text(number)
+    #update.message.reply_text(ans)
+
+def gamename_command(update:Update, context:CallbackContext)->None:
+    logging.info(context.args[0])
+    global name
+    name = context.args[0]
+    number =[]
+    for i in range(0,4):
+        number.append(random.randint(1,10))
+    global ans
+    ans = twentyfour(number)
+    update.message.reply_text(name+',your question is'+str(number).strip('[]')+'!')
+    global start
+    start = time.time()
+    update.message.reply_text('Answer /yes or /no')
+
+
+def yes_command(update:Update, context:CallbackContext)->None:
+    if ans !=False:
+        update.message.reply_text('You win!')
+        update.message.reply_text(ans)
+        global end
+        end = time.time()
+        timeuse = end -start
+        timeuse = round(timeuse, 2)
+        update.message.reply_text('Timeuse:'+str(timeuse))
+        firebase.put('/gamename',name,str(timeuse))
+    if ans ==False:
+        update.message.reply_text('You lose!')
+
+
+def no_command(update:Update, context:CallbackContext)->None:
+    if ans == False:
+        update.message.reply_text('You win!')
+        global end
+        end = time.time()
+        timeuse = end -start
+        timeuse = round(timeuse, 2)
+        update.message.reply_text('Timeuse:'+str(timeuse))
+        firebase.put('/gamename',name,str(timeuse))
+    if ans !=False:
+        update.message.reply_text('You lose!')
+
+
+
+
+
+def twentyfour(cards):
+    for nums in itertools.permutations(cards): # 四个数
+        for ops in itertools.product('+-*/', repeat=3): # 三个运算符（可重复！）
+            # 构造三种中缀表达式 (bsd)
+            bds1 = '({0}{4}{1}){5}({2}{6}{3})'.format(*nums, *ops)  # (a+b)*(c-d)
+            bds2 = '(({0}{4}{1}){5}{2}){6}{3}'.format(*nums, *ops)  # (a+b)*c-d
+            bds3 = '{0}{4}({1}{5}({2}{6}{3}))'.format(*nums, *ops)  #  a/(b-(c/d))
+            
+            for bds in [bds1, bds2, bds3]: # 遍历
+                try:
+                    if abs(eval(bds) - 24.0) < 1e-10:   # eval函数
+                        return bds
+                except ZeroDivisionError: # 零除错误！
+                    continue
+    
+    return 'Not found!'
+
+
+def food_command(update:Update, context:CallbackContext)->None:
+    logging.info(context.args[0])
+    msg = context.args[0]
+    userSeach=msg
+    mainUrl='https://search.bilibili.com/all?keyword='+userSeach
+    mainSoup = BeautifulSoup(requests.get(mainUrl).text, "html.parser")
+    item=mainSoup.find_all('li',class_="video-item matrix")[0]
+    val=item.find('a',class_="img-anchor")
+    
+    subUrl=val["href"];
+    subSoup = BeautifulSoup(requests.get('https:'+subUrl).text.strip(), "html.parser")
+    
+    update.message.reply_text(val["title"])
+    update.message.reply_text(subSoup.find(itemprop="image")["content"])
+    update.message.reply_text('https:'+val["href"])
+
+
+
 if __name__ == '__main__':
     main()
